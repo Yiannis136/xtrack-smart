d7a4b9f072a2697e2194208c936132fa1ce79e9e
Yiannis136 <yiannis_ioannou@hotmail.com>
2025-11-14 11:57:42 +0200

    Update RegisterNow component with a header

--- frontend/src/pages/RegisterNow.js
+++ frontend/src/pages/RegisterNow.js
@@ -1,5 +1,12 @@
 import React from 'react';
+
 const RegisterNow = () => {
-  return <div>Register Now Page</div>;
+  return (
+    <div>
+      <h1>Register Now Page</h1>
+      {/* Add registration form fields here */}
+    </div>
+  );
 };
+
 export default RegisterNow;
